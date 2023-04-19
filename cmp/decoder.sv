0b6efc1ee09c97390eaf0e5c0cd30b6a4b573c9d
foez-ahmed-8041 <foez.official@gmail.com>
2023-04-20 01:00:15 +0600

    new file:   cmp/decoder.sv

---
+++ cmp/decoder.sv
@@ -0,0 +1,14 @@
+module decoder #(
+    parameter CODE_WIDTH = 4
+) (
+    input  logic [CODE_WIDTH-1:0]    code,
+    output logic [2**CODE_WIDTH-1:0] out
+);
+
+    generate
+        for (genvar i=0; i<(2**CODE_WIDTH); i++) begin
+            assign out[i] = (code==i);
+        end
+    endgenerate
+
+endmodule
