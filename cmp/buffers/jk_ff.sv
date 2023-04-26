ca3295a76e21dd30856cc08cf65690b2ff25357c
foez-ahmed-8041 <foez.official@gmail.com>
2023-04-26 10:03:49 +0600

    new file:   cmp/buffers/jk_ff.sv

---
+++ cmp/buffers/jk_ff.sv
@@ -0,0 +1,26 @@
+module jk_ff (
+    input  logic clk_i,
+    input  logic arst_n,
+    input  logic j,
+    input  logic k,
+    output logic q,
+    output logic q_n
+);
+
+    assign q_n = ~q;
+
+    always_ff @(posedge clk_i or negedge arst_n) begin
+        if (~arst_n) begin
+            q <= 0;
+        end
+        else begin
+            case ({j,k})
+                2'b01   : q <= '0;
+                2'b10   : q <= '1;
+                2'b11   : q <= q_n;
+                default : q <= q;
+            endcase
+        end
+    end
+
+endmodule
