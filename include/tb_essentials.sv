a0a3480ae3072681ec3f4cc929df69d5926ad697
foez-ahmed-8041 <foez.official@gmail.com>
2023-05-05 03:53:46 +0600

    modified:   include/tb_essentials.sv modified:   tb/tb_model.sv

--- include/tb_essentials.sv
+++ include/tb_essentials.sv
@@ -8,33 +8,35 @@
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+`define CREATE_CLK(__CLK__, __HIGH__, __LOW__)          \
+    bit ``__CLK__`` = '1;                               \
+    task static start_``__CLK__``();                    \
+        fork                                            \
+            forever begin                               \
+                clk_i = 1; #``__HIGH__``;               \
+                clk_i = 0; #``__LOW__``;                \
+            end                                         \
+        join_none                                       \
+    endtask                                             \
+
+string top_module_name = $sformatf("%m");
+
 initial begin
+    $display("%c[7;38m####################### TEST STARTED #######################%c[0m", 27, 27);
     $dumpfile("raw.vcd");
     $dumpvars;
-    $display("%c[7;38m####################### TEST STARTED #######################%c[0m", 27, 27);
+    repeat (1000) repeat (1000) repeat (1000) #1000;
+    result_print(0, $sformatf("%c[1;31m[FATAL][TIMEOUT]%c[0m", 27, 27));
+    $finish;
 end
+
 final begin
     $display("%c[7;38m######################## TEST ENDED ########################%c[0m", 27, 27);
 end
 
-string top_module_name = $sformatf("%m");
-
 function automatic void result_print (bit PASS, string msg);
-    if (PASS) begin
-        $sformat(msg, "%c[1;32m[PASS]%c[0m %s", 27, 27, msg);
-    end
-    else begin
-        $sformat(msg, "%c[1;31m[FAIL]%c[0m %s", 27, 27, msg);
-    end
+    if (PASS) $sformat(msg, "%c[1;32m[PASS]%c[0m %s", 27, 27, msg);
+    else      $sformat(msg, "%c[1;31m[FAIL]%c[0m %s", 27, 27, msg);
     $sformat(msg, "%s %c[1;33m[%s]%c[0m", msg, 27, top_module_name, 27);
     $display(msg);
 endfunction
-
-// task automatic start_clock (output clk, input int dHIGH = 0, input int dLOW = 0);
-//     fork
-//         forever begin
-//             clk = 1; #((dHIGH==0) ? 5 : dHIGH);
-//             clk = 0; #((dLOW==0) ? ((dHIGH==0) ? 5 : dHIGH) : dLOW);
-//         end
-//     join_none
-// endtask
