6a74c78dd504c7301bcbbf62351b212729392cf0
foez-ahmed-8041 <foez.official@gmail.com>
2023-05-27 23:58:58 +0600

    modified:   intf/axi4l_if.sv

--- intf/axi4l_if.sv
+++ intf/axi4l_if.sv
@@ -2,17 +2,16 @@ interface axi4l_if #(
     parameter type axi_req_t = logic,
     parameter type axi_rsp_t = logic
 ) (
-    input logic clk_i,
-    input logic arst_ni
+    // verilog_linter: off
+    input logic ACLK,
+    input logic ARESETn
+    // verilog_linter: on
 );
 
-  axi_req_t req;
-  axi_rsp_t rsp;
+  // modport manager(input clk_i, input arst_ni, output req, input rsp);
 
-  modport manager(input clk_i, input arst_ni, output req, input rsp);
+  // modport subordinate(input clk_i, input arst_ni, input req, output rsp);
 
-  modport subordinate(input clk_i, input arst_ni, input req, output rsp);
-
-  modport monitor(input clk_i, input arst_ni, input req, input rsp);
+  // modport monitor(input clk_i, input arst_ni, input req, input rsp);
 
 endinterface
