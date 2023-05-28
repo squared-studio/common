378c0bc606f09d63f999d7e0d2ceb87dd536e389
foez-ahmed-8041 <foez.official@gmail.com>
2023-05-28 12:55:32 +0600

    new file:   include/vip/axi4_pkg.sv

---
+++ include/vip/axi4_pkg.sv
@@ -0,0 +1,9 @@
+package axi4_pkg;
+
+  class axi4_driver #(
+    parameter int AddressWidth = 0 
+  );
+  //`define AXI4_T(__NM__, __AW__, __DW__, __IRW__, __IWW__, __UREQW__, __UDTAW__, __URSPW__)
+  endclass
+
+endpackage
