03f2a20e430518e6791cb21ee8397ab98056a431
foez-ahmed-8041 <foez.official@gmail.com>
2023-05-29 16:25:37 +0600

    updated Fix priority arbiter decoder and priority encoder

--- rtl/miscellaneous/decoder.sv
+++ rtl/miscellaneous/decoder.sv
@@ -1,13 +1,19 @@
+// A simple decoder module for translating a code into individual wire set as HIGH
 // ### Author : Foez Ahmed (foez.official@gmail.com)
 
 module decoder #(
-    parameter int CODE_WIDTH = 4
+    parameter int NUM_OUTPUTS = 4  // Code with
 ) (
-    input logic  [   CODE_WIDTH-1:0] code_i,
-    output logic [2**CODE_WIDTH-1:0] out_o
+    input  logic [$clog2(NUM_OUTPUTS)-1:0] code_i, // Code input
+    output logic [        NUM_OUTPUTS-1:0] out_o   // Wire output
 );
 
-  for (genvar i = 0; i < (2 ** CODE_WIDTH); i++) begin : g_outputs
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-ASSIGNMENTS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // Generate output based on code
+  for (genvar i = 0; i < NUM_OUTPUTS; i++) begin : g_outputs
     assign out_o[i] = (code_i == i);
   end
 
--- rtl/miscellaneous/priority_encoder.sv
+++ rtl/miscellaneous/priority_encoder.sv
@@ -1,16 +1,27 @@
+// A simple priority encoder which prioritizes the LSB
 // ### Author : Foez Ahmed (foez.official@gmail.com)
 
 module priority_encoder #(
-    parameter int NUM_INPUTS = 4
+    parameter int NUM_INPUTS = 4 // Number of Inputs
 ) (
-    input  logic [        NUM_INPUTS-1:0] in_i,
-    output logic [$clog2(NUM_INPUTS)-1:0] code_o
+    input  logic [        NUM_INPUTS-1:0] in_i, // Wire input
+    output logic [$clog2(NUM_INPUTS)-1:0] code_o // Code output
 );
 
-  logic [$clog2(NUM_INPUTS)-1:0] out_[NUM_INPUTS];
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-SIGNALS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
 
+  logic [$clog2(NUM_INPUTS)-1:0] out_[NUM_INPUTS]; // finalizing output based on previous
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-ASSIGNMENTS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // Final output
   assign code_o  = out_[NUM_INPUTS-1];
 
+  // MSB calculations
   assign out_[0] = 0;
   for (genvar i = 1; i < NUM_INPUTS; i++) begin : g_msb
     assign out_[i] = in_i[i] ? i : out_[i-1];
