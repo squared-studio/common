459d3cba436980a457c8906b4fa783eab5698353
foez-ahmed-8041 <foez.official@gmail.com>
2023-07-18 20:41:03 +0600

    modified:   rtl/miscellaneous/mux_core.sv

--- rtl/miscellaneous/mux_core.sv
+++ rtl/miscellaneous/mux_core.sv
@@ -4,11 +4,11 @@
 module mux_core #(
     parameter int ELEM_WIDTH = 8,  // Width of each crossbar element
     parameter int NUM_ELEM   = 6   // Number of elements in the crossbar
-) (
-    input  logic [  NUM_ELEM-1:0]                 oe_i,      // Output enable
+  ) (
+    input  logic [$clog2(NUM_ELEM)-1:0]           sel_i,     // Output enable
     input  logic [  NUM_ELEM-1:0][ELEM_WIDTH-1:0] inputs_i,  // Array of input bus
-    output logic [ELEM_WIDTH-1:0]                 outputs_o  // Array of output bus
-);
+    output logic [ELEM_WIDTH-1:0]                 output_o   // Array of output bus
+  );
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-SIGNALS
@@ -20,10 +20,11 @@ module mux_core #(
   //-ASSIGNMENTS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  for (genvar i = 0; i < NUM_ELEM; i++) begin : g_out
-    assign out = oe_i[i] ? inputs_i[i] : 'z;
+  for (genvar i = 0; i < NUM_ELEM; i++)
+  begin : g_out
+    assign out = (sel_i == i) ? inputs_i[i] : 'z;
   end
 
-  assign outputs_o = out;
+  assign output_o = out;
 
 endmodule
