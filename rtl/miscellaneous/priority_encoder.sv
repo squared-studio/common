5dcc45db18d2a07818ada0890e96ac1c1f5a9a05
Foez Ahmed <foez.official@gmail.com>
2023-07-29 21:04:28 +0600

    forgot priority_encoder name itself

--- rtl/miscellaneous/priority_encoder.sv
+++ rtl/miscellaneous/priority_encoder.sv
@@ -5,7 +5,7 @@ module priority_encoder #(
     parameter int NUM_WIRE            = 4,  // Number of output wires
     parameter bit HIGH_INDEX_PRIORITY = 1   // Prioritize Higher index
 ) (
-    input  logic [        NUM_WIRE-1:0] select_i,     // Wire input
+    input  logic [        NUM_WIRE-1:0] d_i,          // Wire input
     output logic [$clog2(NUM_WIRE)-1:0] addr_o,       // Address output
     output logic                        addr_valid_o  // Address Valid output
 );
@@ -22,18 +22,18 @@ module priority_encoder #(
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
   for (genvar i = 0; i < NUM_WIRE; i++) begin : g_allowed_selects
-    assign allowed_selects[i] = select_i[i] & ~select_found[i];
+    assign allowed_selects[i] = d_i[i] & ~select_found[i];
   end
 
   if (HIGH_INDEX_PRIORITY) begin : g_msb_p
     assign select_found[NUM_WIRE-1] = 0;
     for (genvar i = (NUM_WIRE - 2); i <= 0; i--) begin : g_select_found
-      assign select_found[i] = select_found[i+1] | select_i[i+1];
+      assign select_found[i] = select_found[i+1] | d_i[i+1];
     end
   end else begin : g_lsb_p
     assign select_found[0] = 0;
     for (genvar i = 1; i < NUM_WIRE; i++) begin : g_select_found
-      assign select_found[i] = select_found[i-1] | select_i[i-1];
+      assign select_found[i] = select_found[i-1] | d_i[i-1];
     end
   end
 
