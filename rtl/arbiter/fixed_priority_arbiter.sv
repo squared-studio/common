3be19d4103979cb7bbf951695ee337dea0dddf04
Foez Ahmed <foez.official@gmail.com>
2023-08-06 14:17:54 +0600

    Updated fixed priority arbitrary

--- rtl/arbiter/fixed_priority_arbiter.sv
+++ rtl/arbiter/fixed_priority_arbiter.sv
@@ -3,44 +3,35 @@
 module fixed_priority_arbiter #(
     parameter int NUM_REQ = 4
 ) (
-    input  logic               arst_ni,
-    input  logic               allow_req_i,
-    input  logic               en_i,
-    input  logic [NUM_REQ-1:0] req_i,
-    output logic [NUM_REQ-1:0] gnt_o
+    input  logic                       allow_req_i,
+    input  logic [        NUM_REQ-1:0] req_i,
+    output logic [$clog2(NUM_REQ)-1:0] gnt_addr_o,
+    output logic                       gnt_addr_valid_o
 );
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-SIGNALS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  logic [NUM_REQ-1:0] gnt_already_found;
-  logic [NUM_REQ-1:0] latch_out;
+  logic addr_valid_o;
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-ASSIGNMENTS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  assign gnt_already_found[0] = ~allow_req_i;
-  for (genvar i = 1; i < NUM_REQ; i++) begin : g_gnt_found
-    assign gnt_already_found[i] = gnt_already_found[i-1] | gnt_o[i-1];
-  end
-
-  for (genvar i = 0; i < NUM_REQ; i++) begin : g_gnt
-    assign gnt_o[i] = gnt_already_found[i] ? '0 : latch_out[i];
-  end
+  assign gnt_addr_valid_o = allow_req_i & addr_valid_o;
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-RTLS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  latch #(
-      .ELEM_WIDTH(NUM_REQ)
-  ) u_latch_req_i (
-      .arst_ni(arst_ni),
-      .en_i(en_i),
+  priority_encoder #(
+      .NUM_WIRE(NUM_REQ),
+      .HIGH_INDEX_PRIORITY(0)
+  ) priority_encoder_dut (
       .d_i(req_i),
-      .q_o(latch_out)
+      .addr_o(gnt_addr_o),
+      .addr_valid_o(addr_valid_o)
   );
 
 endmodule
