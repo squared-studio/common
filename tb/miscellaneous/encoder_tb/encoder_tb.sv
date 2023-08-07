5b0a000daa8681d392a640472ad512c699c16cd1
Md. Mohiuddin Reyad <65802483+Reyad07@users.noreply.github.com>
2023-08-07 20:17:42 +0600

    17 tb for encoder (#23)
    * added tb for encoder
    
    * updated checker logic - not tested yet
    
    * modified:   tb/miscellaneous/encoder_tb/encoder_tb.sv
    
    * modified:   tb/miscellaneous/encoder_tb/encoder_tb.sv
    
    * modified:   CI_LIST
    modified:   tb/miscellaneous/encoder_tb/encoder_tb.sv
    
    * Updated fixed priority arbitrary
    
    * Changes to be committed:
    modified:   rtl/arbiter/round_robin_arbiter.sv
    modified:   rtl/mem/mem_col.sv
    new file:   rtl/miscellaneous/circular_xbar.sv
    modified:   rtl/miscellaneous/xbar.sv
    modified:   tb/arbiter/fixed_priority_arbiter_tb/fixed_priority_arbiter_tb.sv
    modified:   tb/arbiter/round_robin_arbiter_tb/round_robin_arbiter_tb.sv
    
    * modified:   notes (#20)
    
    modified:   rtl/arbiter/round_robin_arbiter.sv
    renamed:    rtl/mem/mem_col.sv -> rtl/mem/mem.sv
    modified:   rtl/miscellaneous/circular_xbar.sv
    modified:   rtl/miscellaneous/demux.sv
    modified:   rtl/miscellaneous/mux.sv
    modified:   rtl/miscellaneous/xbar.sv
    modified:   tb/miscellaneous/mux_tb/mux_tb.sv
    modified:   tb/miscellaneous/xbar_tb/xbar_tb.sv
    deleted:    todo_ci.md
    
    * modified:   rtl/buffers/fifo/fifo.sv (#22)
    
    modified:   rtl/mem/mem.sv
    
    * Delete todo.md
    
    Signed-off-by: Foez Ahmed <119846826+foez-ahmed-8041@users.noreply.github.com>
    
    * Delete notes
    
    Signed-off-by: Foez Ahmed <119846826+foez-ahmed-8041@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Foez Ahmed <119846826+foez-ahmed-8041@users.noreply.github.com>
    Co-authored-by: Foez Ahmed <foez.official@gmail.com>
    Co-authored-by: Foez Ahmed <119846826+foez-ahmed-8041@users.noreply.github.com>

---
+++ tb/miscellaneous/encoder_tb/encoder_tb.sv
@@ -0,0 +1,106 @@
+// Testbench for encoder
+// ### Author : Md. Mohiuddin Reyad (mreyad30207@gmail.com)
+
+module encoder_tb;
+
+  //`define ENABLE_DUMPFILE
+  //`define DEBUG ;
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-IMPORTS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // bring in the testbench essentials functions and macros
+  `include "vip/tb_ess.sv"
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-LOCALPARAMS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  localparam int NumWire = 8;
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-SIGNALS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  reg  [        NumWire-1:0] d_i;
+  wire [$clog2(NumWire)-1:0] addr_o;
+  wire                       addr_valid_o;
+
+  reg                        or_red;
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-ASSIGNMENTS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  assign or_red = |d_i;
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-RTLS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  encoder #(
+      .NUM_WIRE(NumWire)
+  ) encoder_dut (
+      .d_i(d_i),
+      .addr_o(addr_o),
+      .addr_valid_o(addr_valid_o)
+  );
+
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-PROCEDURALS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  initial begin
+    int pass = 0;
+    int pass_valid = 0;
+    int pass_addr_o = 0;
+    // check :
+    // valid check: if one d_i is high then addr_valid otherwise invalid
+    // do an OR operation for the d_i index value if that particular index is high ---> result will be equal to add_o then PASS otherwise FAIL
+    for (int i = 0; i < 2 ** NumWire; i++) begin
+      int or_ = 0;
+      d_i <= $urandom;
+      #5;
+      foreach (d_i[i]) begin
+`ifdef DEBUG
+        $display("d_i index =%0d", i);
+`endif  //DEBUG
+        if (d_i[i]) begin
+          or_ |= i;
+`ifdef DEBUG
+          $display("d_i[%0d]=%0d", i, d_i[i]);
+`endif  //DEBUG
+        end
+      end
+      #20;
+`ifdef DEBUG  // print or_
+      $display("or_=%0d", or_);
+`endif  //DEBUG
+      if (or_ === addr_o) begin
+        pass++;
+`ifdef DEBUG  //PASS print
+        $display("PASS");
+`endif  //DEBUG
+      end
+      if (or_red === addr_valid_o) begin
+        pass++;
+      end
+`ifdef DEBUG
+      $display("valid= %0d -------addr_valid_o=%0d", valid, addr_valid_o);
+`endif  //DEBUG
+    end
+    #10;
+    //$display("PASS=%0d",pass);
+    if (pass == (2 * (2 ** NumWire))) begin
+      pass = 1;
+    end else begin
+      pass = 0;
+    end
+    result_print(pass, "Encoder Test");
+
+    $finish;
+
+  end
+endmodule
