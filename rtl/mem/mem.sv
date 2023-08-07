eb7084003bf0487842d376d550abda0c4476dba1
Foez Ahmed <119846826+foez-ahmed-8041@users.noreply.github.com>
2023-08-07 10:15:38 +0600

    modified:   rtl/buffers/fifo/fifo.sv (#22)
    modified:   rtl/mem/mem.sv

--- rtl/mem/mem.sv
+++ rtl/mem/mem.sv
@@ -3,32 +3,32 @@
 
 module mem #(
     parameter int ELEM_WIDTH = 8,
-    parameter int NUM_ELEM   = 7
+    parameter int DEPTH   = 7
 ) (
     input logic clk_i,
     input logic arst_ni,
 
     input logic                        we_i,
-    input logic [$clog2(NUM_ELEM)-1:0] waddr_i,
+    input logic [$clog2(DEPTH)-1:0] waddr_i,
     input logic [      ELEM_WIDTH-1:0] wdata_i,
 
-    input  logic [$clog2(NUM_ELEM)-1:0] raddr_i,
+    input  logic [$clog2(DEPTH)-1:0] raddr_i,
     output logic [      ELEM_WIDTH-1:0] rdata_o
 );
 
-  logic [NUM_ELEM-1:0] demux_we;
-  logic [NUM_ELEM-1:0][ELEM_WIDTH-1:0] reg_mux_in;
+  logic [DEPTH-1:0] demux_we;
+  logic [DEPTH-1:0][ELEM_WIDTH-1:0] reg_mux_in;
 
   demux #(
       .ELEM_WIDTH(1),
-      .NUM_ELEM  (NUM_ELEM)
+      .NUM_ELEM  (DEPTH)
   ) u_demux (
       .s_i(waddr_i),
       .i_i(we_i),
       .o_o(demux_we)
   );
 
-  for (genvar i = 0; i < NUM_ELEM; i++) begin : g_reg_array
+  for (genvar i = 0; i < DEPTH; i++) begin : g_reg_array
     register #(
         .ELEM_WIDTH(ELEM_WIDTH)
     ) register_dut (
@@ -42,7 +42,7 @@ module mem #(
 
   mux #(
       .ELEM_WIDTH(ELEM_WIDTH),
-      .NUM_ELEM  (NUM_ELEM)
+      .NUM_ELEM  (DEPTH)
   ) u_mux (
       .s_i(raddr_i),
       .i_i(reg_mux_in),
