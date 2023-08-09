88c1a78e4a4e1151d28744feeb035bf08daa15dd
Reyad07 <mreyad30207@gmail.com>
2023-08-09 21:20:25 +0600

    modified:   rtl/miscellaneous/up_down_counter.sv

--- rtl/buffers/register.sv
+++ rtl/buffers/register.sv
@@ -1,7 +1,8 @@
 // ### Author : Foez Ahmed (foez.official@gmail.com)
 
 module register #(
-    parameter int ELEM_WIDTH = 32
+    parameter int                  ELEM_WIDTH  = 32,
+    parameter bit [ELEM_WIDTH-1:0] RESET_VALUE = '0
 ) (
     input  logic                  clk_i,
     input  logic                  arst_ni,
@@ -12,7 +13,7 @@ module register #(
 
   always_ff @(posedge clk_i or negedge arst_ni) begin
     if (~arst_ni) begin
-      q_o <= '0;
+      q_o <= RESET_VALUE;
     end else if (en_i) begin
       q_o <= d_i;
     end
