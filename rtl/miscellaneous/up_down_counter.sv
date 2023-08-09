fb48f0e89fee579252465aad6ad42790673499fc
Reyad07 <mreyad30207@gmail.com>
2023-08-09 17:35:52 +0600

    up_down counter logic updated

--- rtl/miscellaneous/up_down_counter.sv
+++ rtl/miscellaneous/up_down_counter.sv
@@ -1,5 +1,5 @@
 module up_down_counter #(
-  parameter int COUNT = 16      // max value of count
+  parameter int COUNT = 25      // max value of count
 )(
   input logic                       clk_i,
   input logic                       arstn_i,
@@ -12,11 +12,14 @@ always_ff @(posedge clk_i or negedge arstn_i) begin
   if (~arstn_i) begin
     count_o <= '0;
   end
-  else if (up_i) begin
+  else if (up_i && ~down_i) begin
     count_o++;
   end
-  else if (down_i)begin
+  else if (down_i && ~up_i)begin
     count_o--;
+  end
+  else begin
+    count_o <= '0;
   end
 end
 
