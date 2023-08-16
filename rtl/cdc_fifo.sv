b5478916de3df210c7b271417e4519e2d93ec0ee
Foez Ahmed <foez.official@gmail.com>
2023-08-16 20:30:02 +0600

    added cdc_fifo and removed cdc_pipeline modified:   diagrams/cdc_fifo.drawio deleted:    diagrams/cdc_pipeline.drawio modified:   rtl/bin_to_gray.sv modified:   rtl/cdc_fifo.sv deleted:    rtl/cdc_pipeline.sv renamed:    tb/cdc_pipeline_tb/cdc_pipeline_tb.sv -> tb/cdc_fifo_tb/cdc_fifo_tb.sv renamed:    tb/cdc_pipeline_tb/vivado_compile_extra_command_line_options -> tb/cdc_fifo_tb/vivado_compile_extra_command_line_options renamed:    tb/cdc_pipeline_tb/vivado_elaborate_extra_command_line_options -> tb/cdc_fifo_tb/vivado_elaborate_extra_command_line_options renamed:    tb/cdc_pipeline_tb/vivado_stimulate_extra_command_line_options -> tb/cdc_fifo_tb/vivado_stimulate_extra_command_line_options new file:   tb/cdc_fifo_tb/wave.gtkw deleted:    tb/cdc_pipeline_tb/wave.gtkw

--- rtl/cdc_fifo.sv
+++ rtl/cdc_fifo.sv
@@ -1,5 +1,5 @@
-// Description
-// ### Author : name (email)
+// Clock Domain Crossing FIFO
+// ### Author : Foez Ahmed (foez.official@gmail.com)
 
 module cdc_fifo #(
     parameter int ELEM_WIDTH = 8,  // Element width
@@ -28,31 +28,147 @@ module cdc_fifo #(
   //-SIGNALS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
+  logic [FIFO_SIZE:0] wr_ptr_pass;  // Passing write pointer gray across clock domain
+  logic [FIFO_SIZE:0] rd_ptr_pass;  // Passing read pointer gray across clock domain
 
+  logic hsi;  // input handshake
+  logic hso;  // output handshake
+
+  logic [FIFO_SIZE:0] wr_addr;  // write address
+  logic [FIFO_SIZE:0] rd_addr;  // read address
+
+  logic [FIFO_SIZE:0] wr_addr_;  // write address on other clock
+  logic [FIFO_SIZE:0] rd_addr_;  // read address on other clock
+
+  logic [FIFO_SIZE:0] wr_addr_p1;  // write address +1
+  logic [FIFO_SIZE:0] rd_addr_p1;  // read address +1
+
+  logic [FIFO_SIZE:0] wpgi;  // write pointer gray in to the reg
+  logic [FIFO_SIZE:0] rpgi;  // read pointer gray in to the reg
+
+  logic [FIFO_SIZE:0] wpgo;  // write pointer gray out from the reg
+  logic [FIFO_SIZE:0] rpgo;  // read pointer gray out from the reg
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-ASSIGNMENTS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
+  assign hsi = elem_in_valid_i & elem_in_ready_o;
+  assign hso = elem_out_valid_o & elem_out_ready_i;
 
+  assign wr_addr_p1 = wr_addr + 1;
+  assign rd_addr_p1 = rd_addr + 1;
+
+  assign elem_in_ready_o = !(
+                              (wr_addr[FIFO_SIZE] != rd_addr_[FIFO_SIZE])
+                              &&
+                              (wr_addr[FIFO_SIZE-1:0] == rd_addr_[FIFO_SIZE-1:0])
+                            );
+
+  assign elem_out_valid_o = (wr_addr_ != rd_addr);
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-RTLS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
+  gray_to_bin #(
+      .DATA_WIDTH(FIFO_SIZE + 1)
+  ) g2b_wi (
+      .data_in_i (wr_ptr_pass),
+      .data_out_o(wr_addr)
+  );
 
+  gray_to_bin #(
+      .DATA_WIDTH(FIFO_SIZE + 1)
+  ) g2b_ri (
+      .data_in_i (rpgo),
+      .data_out_o(rd_addr_)
+  );
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-METHODS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
+  gray_to_bin #(
+      .DATA_WIDTH(FIFO_SIZE + 1)
+  ) g2b_wo (
+      .data_in_i (wpgo),
+      .data_out_o(wr_addr_)
+  );
 
+  gray_to_bin #(
+      .DATA_WIDTH(FIFO_SIZE + 1)
+  ) g2b_ro (
+      .data_in_i (rd_ptr_pass),
+      .data_out_o(rd_addr)
+  );
 
+  bin_to_gray #(
+      .DATA_WIDTH(FIFO_SIZE + 1)
+  ) b2g_w (
+      .data_in_i (wr_addr_p1),
+      .data_out_o(wpgi)
+  );
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-SEQUENCIALS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
+  bin_to_gray #(
+      .DATA_WIDTH(FIFO_SIZE + 1)
+  ) b2g_r (
+      .data_in_i (rd_addr_p1),
+      .data_out_o(rpgi)
+  );
 
+  register #(
+      .ELEM_WIDTH (FIFO_SIZE + 1),
+      .RESET_VALUE('0)
+  ) wr_ptr_ic (
+      .clk_i  (elem_in_clk_i),
+      .arst_ni(arst_ni),
+      .en_i   (hsi),
+      .d_i    (wpgi),
+      .q_o    (wr_ptr_pass)
+  );
 
+  register #(
+      .ELEM_WIDTH (FIFO_SIZE + 1),
+      .RESET_VALUE('0)
+  ) rd_ptr_ic (
+      .clk_i  (elem_in_clk_i),
+      .arst_ni(arst_ni),
+      .en_i   ('1),
+      .d_i    (rd_ptr_pass),
+      .q_o    (rpgo)
+  );
+
+  register #(
+      .ELEM_WIDTH (FIFO_SIZE + 1),
+      .RESET_VALUE('0)
+  ) wr_ptr_oc (
+      .clk_i  (elem_out_clk_i),
+      .arst_ni(arst_ni),
+      .en_i   ('1),
+      .d_i    (wr_ptr_pass),
+      .q_o    (wpgo)
+  );
+
+  register #(
+      .ELEM_WIDTH (FIFO_SIZE + 1),
+      .RESET_VALUE('0)
+  ) rd_ptr_oc (
+      .clk_i  (elem_out_clk_i),
+      .arst_ni(arst_ni),
+      .en_i   (hso),
+      .d_i    (rpgi),
+      .q_o    (rd_ptr_pass)
+  );
+
+  mem #(
+      .ELEM_WIDTH(ELEM_WIDTH),
+      .DEPTH(2 ** FIFO_SIZE)
+  ) u_mem (
+      .clk_i  (elem_in_clk_i),
+      .arst_ni(arst_ni),
+      .we_i   (hsi),
+      .waddr_i(wr_addr),
+      .wdata_i(elem_in_i),
+      .raddr_i(rd_addr),
+      .rdata_o(elem_out_o)
+  );
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-INITIAL CHECKS
@@ -60,8 +176,8 @@ module cdc_fifo #(
 
 `ifdef SIMULATION
   initial begin
-    if (DATA_WIDTH > 2) begin
-      $display("\033[7;31m%m DATA_WIDTH\033[0m");
+    if (FIFO_SIZE > 3) begin
+      $display("\033[7;31m%m FIFO_SIZE=%0d is quite big\033[0m", FIFO_SIZE);
     end
   end
 `endif  // SIMULATION
