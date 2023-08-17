6da0be4758f5af2c0a0261854f65aad5832e49cb
Foez Ahmed <119846826+foez-ahmed-8041@users.noreply.github.com>
2023-08-17 22:35:23 +0600

    modified:   include/vip/tb_ess.sv (#36)
    modified:   rtl/counter.sv

--- rtl/counter.sv
+++ rtl/counter.sv
@@ -2,7 +2,7 @@
 // ### Author : Md. Mohiuddin Reyad (mreyad30207@gmail.com)
 
 module counter #(
-    parameter int                           MAX_COUNT   = 25,  // max value of count
+    parameter int                           MAX_COUNT   = 25,
     parameter bit [$clog2(MAX_COUNT+1)-1:0] RESET_VALUE = '0,
     parameter bit                           UP_COUNT    = 1,
     parameter bit                           DOWN_COUNT  = 1
@@ -14,54 +14,41 @@ module counter #(
     output logic [$clog2(MAX_COUNT+1)-1:0] count_o
 );
 
-  logic [$clog2(MAX_COUNT+1)-1:0] mux_out;
-
-  logic [1:0][$clog2(MAX_COUNT+1)-1:0] mux_in;
-
-
-  logic [$clog2(MAX_COUNT+1)-1:0] delta;
-
-  logic [1:0] mux_sel;
-
-
-  //if (UP_COUNT && DOWN_COUNT) begin : g_both_ud
-
-  assign delta = down_i ? '1 : 1;
-
-  assign mux_in[0] = '0;
-  assign mux_sel[0] = (count_o == MAX_COUNT) && up_i && !down_i;
-
-  assign mux_in[1] = MAX_COUNT;
-  assign mux_sel[1] = (count_o == 0) && !up_i && down_i;
-
-  assign mux_in[2] = count_o + delta;
-  assign mux_sel[2] = (up_i != down_i);
-
-  assign mux_in[3] = count_o;
-  assign mux_sel[3] = (up_i == down_i);
-
-  mux_primitive #(
-      .ELEM_WIDTH($clog2(MAX_COUNT + 1)),
-      .NUM_ELEM  (4)
-  ) mux_primitive_dut (
-      .s_i(mux_sel),
-      .i_i(mux_in),
-      .o_o(mux_out)
-  );
-
-  //end
-
-
-  register #(
-      .ELEM_WIDTH ($clog2(MAX_COUNT + 1)),
-      .RESET_VALUE(RESET_VALUE)
-  ) register_dut (
-      .clk_i(clk_i),
-      .arst_ni(arst_ni),
-      .en_i('1),
-      .d_i(mux_out),
-      .q_o(count_o)
-  );
+  if (UP_COUNT && DOWN_COUNT) begin : g_up_down
+    always_ff @(posedge clk_i or negedge arst_ni) begin
+      if (~arst_ni) begin
+        count_o <= RESET_VALUE;
+      end else if (up_i ^ down_i) begin
+        if (up_i) begin
+          if (count_o == MAX_COUNT) count_o <= '0;
+          else count_o <= count_o + 1;
+        end else begin
+          if (count_o == '0) count_o <= MAX_COUNT;
+          else count_o <= count_o - 1;
+        end
+      end
+    end
+  end else if (UP_COUNT && !DOWN_COUNT) begin : g_up
+    always_ff @(posedge clk_i or negedge arst_ni) begin
+      if (~arst_ni) begin
+        count_o <= RESET_VALUE;
+      end else if (up_i) begin
+        if (count_o == MAX_COUNT) count_o <= '0;
+        else count_o <= count_o + 1;
+      end
+    end
+  end else if (!UP_COUNT && DOWN_COUNT) begin : g_down
+    always_ff @(posedge clk_i or negedge arst_ni) begin
+      if (~arst_ni) begin
+        count_o <= RESET_VALUE;
+      end else if (down_i) begin
+        if (count_o == '0) count_o <= MAX_COUNT;
+        else count_o <= count_o - 1;
+      end
+    end
+  end else begin : g_nothing
+    assign count_o = '0;
+  end
 
 endmodule
 
