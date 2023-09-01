a3585e3457f91fb72e51f875cd14e3b4b5bb79a7
Foez Ahmed <foez.official@gmail.com>
2023-09-01 17:48:42 +0600

    modified:   rtl/model/pll.sv new file:   tb/pll_tb/config/default/xelab new file:   tb/pll_tb/config/default/xsim new file:   tb/pll_tb/config/default/xvlog new file:   tb/pll_tb/pll_tb.sv new file:   tb/pll_tb/wave.gtkw

--- rtl/pll.sv
+++ rtl/pll.sv
@@ -1,3 +1,6 @@
+// Phase Lock Loop circuit model
+// ### Author : Foez Ahmed (foez.official@gmail.com)
+
 module pll (
     input logic bypass_i,
 
@@ -16,7 +19,7 @@ module pll (
   realtime old_fref_tick = 0ns;
   realtime new_fref_tick = 100ns;
   realtime fref_time_period = 0;
-  realtime fvco_time_period = 1us;
+  realtime fvco_time_period = 0;
 
   logic fout_ff;
 
@@ -35,14 +38,23 @@ module pll (
   end
 
   always @(posedge fvco_o) begin
-    lock_o <= (fref_time_period / (fvco_time_period * fbdiv_i)) inside {[0.999 : 1.001]};
+    if (fvco_time_period == 0) begin
+      lock_o <= '0;
+    end else begin
+      lock_o <= (fref_time_period / (fvco_time_period * fbdiv_i)) inside {[0.999 : 1.001]};
+    end
   end
 
   always begin
-    fvco_o <= '1;
-    #(fvco_time_period / 2);
-    fvco_o <= '0;
-    #(fvco_time_period / 2);
+    if (fvco_time_period == 0) begin
+      @(fref_i);
+      fvco_o <= fref_i;
+    end else begin
+      fvco_o <= '1;
+      #(fvco_time_period / 2);
+      fvco_o <= '0;
+      #(fvco_time_period / 2);
+    end
   end
 
   always begin
