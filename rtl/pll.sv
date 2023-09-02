531ce04ee5f3b3e1154e02c800b95ddf198d8d0e
Foez Ahmed <119846826+foez-ahmed-8041@users.noreply.github.com>
2023-09-02 12:22:45 +0600

    111 pll (#118)
    * modified:   rtl/model/pll.sv
    new file:   tb/pll_tb/config/default/xelab
    new file:   tb/pll_tb/config/default/xsim
    new file:   tb/pll_tb/config/default/xvlog
    new file:   tb/pll_tb/pll_tb.sv
    new file:   tb/pll_tb/wave.gtkw
    
    * modified:   rtl/model/pll.sv
    modified:   tb/pll_tb/pll_tb.sv
    modified:   tb/pll_tb/wave.gtkw
    
    * Changes to be committed:
    renamed:    rtl/model/pll.sv -> rtl/pll.sv
    
    * modified:   rtl/model/pll.sv
    new file:   tb/pll_tb/config/default/xelab
    new file:   tb/pll_tb/config/default/xsim
    new file:   tb/pll_tb/config/default/xvlog
    new file:   tb/pll_tb/pll_tb.sv
    new file:   tb/pll_tb/wave.gtkw
    
    * modified:   rtl/model/pll.sv
    modified:   tb/pll_tb/pll_tb.sv
    modified:   tb/pll_tb/wave.gtkw
    
    * modified:   rtl/pll.sv
    modified:   tb/pll_tb/pll_tb.sv
    modified:   tb/pll_tb/wave.gtkw
    
    * modified:   tb/pll_tb/wave.gtkw
    
    ---------
    
    Signed-off-by: Foez Ahmed <119846826+foez-ahmed-8041@users.noreply.github.com>

--- rtl/pll.sv
+++ rtl/pll.sv
@@ -1,43 +1,65 @@
+// This is a non-synthesizable model of the PLL circuit, designed for verification purpose only.
+// ### Author : Foez Ahmed (foez.official@gmail.com)
+
 module pll (
-    input logic bypass_i,
+    input logic bypass_i,  // bypass the reference frequency to fout_o
 
-    input logic       fref_i,
-    input logic [7:0] refdiv_i,
+    input logic       fref_i,   // reference frequency
+    input logic [7:0] refdiv_i, // reference frequency divider
 
-    output logic lock_o,
+    output logic lock_o,  // PLL lock
 
-    input  logic [7:0] fbdiv_i,
-    output logic       fvco_o,
+    input  logic [15:0] fbdiv_i,  // VCO feedback voltage divider
+    output logic        fvco_o,   // VCO frequency
 
-    input  logic [7:0] fdiv_i,
-    output logic       fout_o
+    input  logic [7:0] fdiv_i,  // output frequency divider
+    output logic       fout_o   // output frequency
 );
 
-  realtime old_fref_tick = 0ns;
-  realtime new_fref_tick = 100ns;
-  realtime fref_time_period = 0;
-  realtime fvco_time_period = 1us;
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-VARIABLES
+  //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  logic fout_ff;
+  realtime fref_time_period = 0;  // Time period of fref_i after division
+  realtime fvco_time_period = 1us;  // Time period of fvco_o
+  realtime old_fref_tick = 0ns;  // divided fref_i previous posedge time
+  realtime new_fref_tick = 100ns;  // divided fref_i current posedge time
 
-  initial begin
-    $timeformat(-12, 3, "ps");
-  end
+  logic fout_ff;  // divided fvco_o
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-ASSIGNMENTS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // if bypassed, switch to fref_i
+  // Otherwise if fdiv is 1, directly pass fvco as there is no need of frequency division,
+  // else pass the divided fvoc_o
+  assign fout_o = bypass_i ? fref_i : ((fdiv_i == 1) ? fvco_o : fout_ff);
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-PROCEDURALS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
 
   always @(posedge fref_i) begin
-    int rpt;
+
+    // handling the refdiv here. Wait additional cycles for calculating time period
+    automatic int rpt;
     rpt = (refdiv_i > 0) ? (refdiv_i - 1) : 1;
     repeat (rpt) @(posedge fref_i);
+
+    // Calculating time period
     new_fref_tick = $realtime;
     fref_time_period = (new_fref_tick - old_fref_tick);
-    fvco_time_period = fvco_time_period * 0.99 + 0.01 * (fref_time_period / fbdiv_i);
     old_fref_tick = new_fref_tick;
-  end
 
-  always @(posedge fvco_o) begin
+    // Digital low pass fileted for soomthly transitioning frequency
+    fvco_time_period = fvco_time_period * 0.9 + 0.1 * (fref_time_period / fbdiv_i);
+
+    // Determine if desired frequency is achieved
     lock_o <= (fref_time_period / (fvco_time_period * fbdiv_i)) inside {[0.999 : 1.001]};
   end
 
+  // Toggling fvco_o
   always begin
     fvco_o <= '1;
     #(fvco_time_period / 2);
@@ -45,9 +67,10 @@ module pll (
     #(fvco_time_period / 2);
   end
 
+  // dividing the fvco_o by fdiv to get fout_o
   always begin
-    int hc;
-    int lc;
+    automatic int hc;
+    automatic int lc;
     hc = fdiv_i[7:1];
     lc = (fdiv_i[0]) ? (fdiv_i[7:1] + 1) : (fdiv_i[7:1]);
     fout_ff <= '1;
@@ -56,12 +79,9 @@ module pll (
     repeat (lc) @(posedge fvco_o);
   end
 
-`ifdef DEBUG
-  always #1us begin
-    $display("I:%t O:%t", (fref_time_period / fref_i), fvco_time_period);
+  // defining time format
+  initial begin
+    $timeformat(-12, 3, "ps");
   end
-`endif  // DEBUG
-
-  assign fout_o = (fdiv_i == 1) ? fvco_o : fout_ff;
 
 endmodule
