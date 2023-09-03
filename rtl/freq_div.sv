56808a74c7e7e18196cdcf505c51d8d07ef3d09d
Foez Ahmed <foez.official@gmail.com>
2023-09-03 22:21:30 +0600

    modified:   rtl/freq_div.sv

--- rtl/freq_div.sv
+++ rtl/freq_div.sv
@@ -1,58 +1,64 @@
+// Frequency Divider
+// ### Author : Foez Ahmed (foez.official@gmail.com)
+
 module freq_div #(
-    parameter int DIVISOR = 3
+    parameter int DIVISOR_SIZE = 9  // divisor register size
 ) (
-    input  logic arst_ni,
-    input  logic clk_i,
-    output logic clk_o
+    input  logic                    arst_ni,    // Asynchronous Global Reset
+    input  logic [DIVISOR_SIZE-1:0] divisor_i,  // clock divisor
+    input  logic                    clk_i,      // clock in
+    output logic                    clk_o       // clock out
 );
 
-  if (DIVISOR < 2) begin : g_bypass
-    assign clk_o = clk_i;
-  end else begin : g_div
-    localparam int EQ = !(DIVISOR % 2);
-    localparam int HC = (DIVISOR >> 1);
-    localparam int LC = EQ ? (DIVISOR >> 1) : ((DIVISOR >> 1) + 1);
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-SIGNALS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
 
-    logic [$clog2(DIVISOR+1)-1:0] count;
-    logic [$clog2(DIVISOR+1)-1:0] count_next;
+  logic bypass;  // bypass when divisor is 1 or 0
+  logic clk;  // output clock if not bypassed
 
-    assign count_next = count + 1;
+  logic [DIVISOR_SIZE-1:0] count;  // counter for frequency division
+  logic [DIVISOR_SIZE-1:0] count_p1;  // counter value+1
 
-    if (EQ) begin : g_EQ
-      always_ff @(posedge clk_i or negedge arst_ni) begin
-        if (~arst_ni) begin
+  logic [DIVISOR_SIZE-1:0] hct;  // high count threshold
+  logic [DIVISOR_SIZE-1:0] lct;  // low count threshold
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-ASSIGNMENTS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  assign bypass = (divisor_i < 2);
+  assign count_p1 = count + 1;
+
+  // round down division by 2
+  assign hct = divisor_i[DIVISOR_SIZE-1:1];
+  // increase the lct for odd divisor
+  assign lct = divisor_i[DIVISOR_SIZE-1:1] + divisor_i[0];
+
+  assign clk_o = bypass ? clk_i : clk;
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-SEQUENTIAL
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  always_ff @(posedge clk_i or negedge arst_ni) begin
+    if (~arst_ni) begin
+      clk   <= '0;
+      count <= '1;
+    end else begin
+      if (clk == 0) begin
+        if (count_p1 == lct) begin
           count <= '0;
-          clk_o <= '0;
+          clk   <= '1;
         end else begin
-          if (count_next == LC) begin
-            clk_o <= ~clk_o;
-            count <= '0;
-          end else begin
-            count <= count_next;
-          end
+          count <= count_p1;
         end
-      end
-    end else begin : g_nEQ
-      always_ff @(posedge clk_i or negedge arst_ni) begin
-        if (~arst_ni) begin
+      end else begin
+        if (count_p1 == hct) begin
           count <= '0;
-          clk_o <= '0;
+          clk   <= '0;
         end else begin
-          if (clk_o) begin
-            if (count_next == HC) begin
-              clk_o <= '0;
-              count <= '0;
-            end else begin
-              count <= count_next;
-            end
-          end else begin
-            if (count_next == LC) begin
-              clk_o <= '1;
-              count <= '0;
-            end else begin
-              count <= count_next;
-            end
-          end
+          count <= count_p1;
         end
       end
     end
