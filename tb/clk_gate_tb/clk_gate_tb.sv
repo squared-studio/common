0206a8467442bdd73113287c44d3e90736f0ce00
mdrazuahamed <en.razu.ahamed@gmail.com>
2023-09-04 13:00:55 +0600

    solving commenting issue

--- tb/clk_gate_tb/clk_gate_tb.sv
+++ tb/clk_gate_tb/clk_gate_tb.sv
@@ -1,25 +1,25 @@
 // Simple test bench for clk_gate module
 // ### Author : Razu Ahamed(en.razu.ahamed@gmail.com)
 
-module tb_clk_gate;
+module clk_gate_tb;
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-SIGNALS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  logic cp_i;  //Clock pulse
-  logic e_i;  //Enable
-  logic te_i;  //Test enable
-  logic q_o;  //Output
-  logic q_model;
-  int   pass = 0;//Declear a signal and initializing with 0
-  int   fail = 0;//Declear a signal and initializing with 0
+  logic cp_i;  // Clock pulse
+  logic e_i;  // Enable
+  logic te_i;  // Test enable
+  logic q_o;  // Output
+  logic q_model;  // Store the reference model data
+  int   pass = 0;  // Declare a signal and initializing with 0
+  int   fail = 0;  // Declare a signal and initializing with 0
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-RTL
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  //Dut instantiation
+  // Dut instantiation
   clk_gate u_clk_gate (
       .cp_i(cp_i),
       .e_i (e_i),
@@ -31,11 +31,11 @@ module tb_clk_gate;
   //-Method
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  //Make a reference Model using function
+  // Make a reference Model using function
   function automatic logic q_out(logic cp_i, logic e_i, logic te_i);
-    logic temp;
-    temp = cp_i & e_i;
-    if (te_i == 1) q_out = cp_i;
+    logic temp;                  // Declare a signal to store output of AND operation
+    temp = cp_i & e_i;           // Storeing AND operation data
+    if (te_i == 1) q_out = cp_i; // Creating 2to1 MUX using if condition
     else q_out = temp;
   endfunction
 
@@ -43,7 +43,7 @@ module tb_clk_gate;
   //-Procedural
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  //Generate dump.vcd file for waveform
+  // Generate dump.vcd file for waveform
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars;
@@ -51,17 +51,20 @@ module tb_clk_gate;
 
   initial begin
     repeat (200) begin
+
       // Randomize data and call reference model function
       cp_i <= $urandom;
       te_i <= $urandom;
       e_i  <= $urandom;
       #1;
       q_model = q_out(cp_i, e_i, te_i);
+
       // Compare actual data with expected data that come from reference model
-      if (q_o == q_model) pass++;  // counting match time
-      else fail++;
+      if (q_o == q_model) pass++; // counting matched time
+      else fail++;                // counting mismatched time
     end
-    // Dispaly data to know how many time data is passed and failed
+
+    // Display data to know how many time data is passed and failed
     $display("%d Time Passed", pass);
     $display("%d Time Failed", fail);
     #100 $finish;
