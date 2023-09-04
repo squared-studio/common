cd078221ca3bddcef31434260c99b65bb6ebc34e
Razu Ahamed <en.razu.ahamed@gmail.com>
2023-09-04 23:51:03 +0600

    65 gray to bin tbsv (#126)
    * Add files via upload
    
    Signed-off-by: mdrazuahamed <en.razu.ahamed@gmail.com>
    
    * cleanup gray_to_bin_tb
    
    * clean up gray_to_bin_tb
    
    ---------
    
    Signed-off-by: mdrazuahamed <en.razu.ahamed@gmail.com>

--- tb/gray_to_bin_tb/gray_to_bin_tb.sv
+++ tb/gray_to_bin_tb/gray_to_bin_tb.sv
@@ -1,3 +1,4 @@
+// Simple testbench for Gray to Binary code converter
 // ### Author : Razu Ahamed (engr.razu.ahamed@gmail.com)
 
 module gray_to_bin_tb;
@@ -6,19 +7,21 @@ module gray_to_bin_tb;
   //-IMPORTS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
+  // Include tb_ess.sv file
   `include "vip/tb_ess.sv"
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-LOCALPARAMS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
+  // Declare a local parameter that defines the depth of input and output data
   localparam int DataWidth = 11;
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-SIGNALS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+  // Decleare two singals for input and output
   logic [DataWidth-1:0] data_in_i;
   logic [DataWidth-1:0] data_out_o;
 
@@ -26,6 +29,7 @@ module gray_to_bin_tb;
   //-RTLS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
+  // Instantiate the DUT
   gray_to_bin #(
       .DATA_WIDTH(DataWidth)
   ) gray_to_bin_dut (
@@ -37,6 +41,7 @@ module gray_to_bin_tb;
   //-METHODS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
+  // Make reference model
   function automatic logic [DataWidth-1:0] data_out_gray_to_bin(logic [DataWidth-1:0] data_in);
     data_out_gray_to_bin[DataWidth-1] = data_in[DataWidth-1];
     for (int i = DataWidth - 2; i >= 0; i--) begin
@@ -49,21 +54,18 @@ module gray_to_bin_tb;
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
   initial begin
-
-    static int fail = 0;
-    static int pass = 0;
+    static int fail = 0;  // Deleare a signal for counting how many time data are matching
+    static int pass = 0;  // Declare a signal for counting how many time data are mismatching
 
     for (int i = 0; i < 2 ** DataWidth; i++) begin
-      data_in_i <= $urandom;
-      #1;
+      data_in_i <= $urandom;  //Randomizing the input data
+      #1;                     // added 1 time unit delay
+      // Calling the function and compare actual data with expected data
       if (data_out_o !== data_out_gray_to_bin(data_in_i)) fail++;
       else pass++;
     end
-
+    // Display passed and failed ratio
     result_print(!fail, $sformatf("data conversion %0d/%0d", pass, pass + fail));
-
-    $finish;
-
+    $finish;  // Terminate the simulation
   end
-
 endmodule
