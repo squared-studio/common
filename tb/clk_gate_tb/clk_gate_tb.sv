e522967550e338574336a2d43dcff5751aba9d55
mdrazuahamed <en.razu.ahamed@gmail.com>
2023-09-04 16:59:03 +0600

    Update clk_date testbench

--- tb/clk_gate_tb/clk_gate_tb.sv
+++ tb/clk_gate_tb/clk_gate_tb.sv
@@ -4,16 +4,22 @@
 module clk_gate_tb;
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-IMPORTS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  `include "vip/tb_ess.sv"
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
   //-SIGNALS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  logic cp_i;     // Clock pulse
-  logic e_i;      // Enable
-  logic te_i;     // Test enable
-  logic q_o;      // Output
-  logic q_model;  // Store the reference model data
-  int   pass = 0; // Declare a signal and initializing with 0
-  int   fail = 0; // Declare a signal and initializing with 0
+  logic cp_i;      // Clock pulse
+  logic e_i;       // Enable
+  logic te_i;      // Test enable
+  logic q_o;       // Output
+  logic q_model;   // Store the reference model data
+  int   pass = 0;  // Declare a signal and initializing with 0
+  int   fail = 0;  // Declare a signal and initializing with 0
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-RTL
@@ -33,9 +39,9 @@ module clk_gate_tb;
 
   // Make a reference Model using function
   function automatic logic q_out(logic cp_i, logic e_i, logic te_i);
-    logic temp;                  // Declare a signal to store output of AND operation
-    temp = cp_i & e_i;           // Storeing AND operation data
-    if (te_i == 1) q_out = cp_i; // Creating 2to1 MUX using if condition
+    logic temp;                   // Declare a signal to store output of AND operation
+    temp = cp_i & e_i;            // Storeing AND operation data
+    if (te_i == 1) q_out = cp_i;  // Creating 2to1 MUX using if condition
     else q_out = temp;
   endfunction
 
@@ -60,13 +66,12 @@ module clk_gate_tb;
       q_model = q_out(cp_i, e_i, te_i);
 
       // Compare actual data with expected data that come from reference model
-      if (q_o == q_model) pass++; // counting matched time
-      else fail++;                // counting mismatched time
+      if (q_o == q_model) pass++;  // counting matched time
+      else fail++;                 // counting mismatched time
     end
 
-    // Display data to know how many time data is passed and failed
-    $display("%d Time Passed", pass);
-    $display("%d Time Failed", fail);
+    // Display to know how many time data is passed and failed
+    result_print(!fail, $sformatf("data conversion %0d/%0d", pass, pass + fail));
     #100 $finish;
   end
 endmodule
