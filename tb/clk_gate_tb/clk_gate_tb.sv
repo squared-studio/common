06f89bd3d426bc53452d02982a4a61111bfb6d51
mdrazuahamed <en.razu.ahamed@gmail.com>
2023-09-05 11:25:24 +0600

    add driver monitor

--- tb/clk_gate_tb/clk_gate_tb.sv
+++ tb/clk_gate_tb/clk_gate_tb.sv
@@ -13,14 +13,13 @@ module clk_gate_tb;
   //-SIGNALS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  logic cp_i;      // Clock pulse
-  logic e_i;       // Enable
-  logic te_i;      // Test enable
-  logic q_o;       // Output
-  logic q_model;   // Store the reference model data
-  int   pass = 0;  // Declare a signal and initializing with 0
-  int   fail = 0;  // Declare a signal and initializing with 0
-
+  `CREATE_CLK(clk_i, 5ns, 5ns)
+  logic cp_i  =0 ;  // Clock pulse
+  logic e_i   =0 ;  // Enable
+  logic te_i  =0 ;  // Test enable
+  logic q_o      ;  // Output
+  int   pass  = 0;  // Declare a signal for counting match and initializing with 0
+  int   fail  = 0;  // Declare a signal for counting mismatch and initializing with 0
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-RTL
   //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -45,6 +44,28 @@ module clk_gate_tb;
     else q_out = temp;
   endfunction
 
+  // Driver for randomized stimuluses
+  task static drive();
+    fork
+      forever
+      @(posedge clk_i) begin
+        cp_i <= $urandom;
+        te_i <= $urandom;
+        e_i  <= $urandom;
+      end
+    join_none
+  endtask
+
+  // Monitoring and Scoreboard
+  task static monitor_scoreboard();
+    fork
+      forever
+      @(posedge clk_i) begin
+        if (q_o == q_out(cp_i, e_i, te_i)) pass++;
+        else if (q_o != q_out(cp_i, e_i, te_i)) fail++;
+      end
+    join_none
+  endtask
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-Procedural
   //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -56,22 +77,16 @@ module clk_gate_tb;
   end
 
   initial begin
-    repeat (200) begin
-
-      // Randomize data and call reference model function
-      cp_i <= $urandom;
-      te_i <= $urandom;
-      e_i  <= $urandom;
-      #1;
-      q_model = q_out(cp_i, e_i, te_i);
-
-      // Compare actual data with expected data that come from reference model
-      if (q_o == q_model) pass++;  // counting matched time
-      else fail++;                 // counting mismatched time
+    start_clk_i();
+    drive();
+    monitor_scoreboard();
+    //  Repeating the Test for N number of test cases - - - - > N = 100
+    for (int k = 0; k < 100; k++) begin
+      @(posedge clk_i);
     end
 
-    // Display to know how many time data is passed and failed
-    result_print(!fail, $sformatf("data conversion %0d/%0d", pass, pass + fail));
-    #100 $finish;
+    //  Display result to know how many time data is passed and failed
+    result_print(!fail, $sformatf("data match ratio (pass/(pass+fail))= %0d/%0d", pass, pass + fail));
+    $finish;
   end
 endmodule
