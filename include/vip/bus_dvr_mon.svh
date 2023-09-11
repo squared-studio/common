09f062f2caa3d784c53182fd4f5477a4a4845301
Foez Ahmed <foez.official@gmail.com>
2023-09-11 21:47:39 +0600

    modified:   include/vip/bus_dvr_mon.svh modified:   include/vip/tb_ess.sv

--- include/vip/bus_dvr_mon.svh
+++ include/vip/bus_dvr_mon.svh
@@ -2,37 +2,84 @@
 
 
 `define HANDSHAKE_SEND_RECV_LOOK(__NAME__, __TYPE__, __clk__, __CHAN__, __VALID__, __READY__)      \
+  /*--------------------------------------------------------------------------------------------*/ \
+  int ``__NAME__``_send_id = 0;                                                                    \
+  int ``__NAME__``_send_queue  [$];                                                                \
   task automatic send_``__NAME__``(input ``__TYPE__`` beat);                                       \
+    int thread_id;                                                                                 \
+    thread_id = ``__NAME__``_send_id;                                                              \
+    ``__NAME__``_send_id++;                                                                        \
+    ``__NAME__``_send_queue.push_back(thread_id);                                                  \
+    while (``__NAME__``_send_queue[0] != thread_id) @(posedge ``__clk__``);                        \
     ``__CHAN__``  <= beat;                                                                         \
     ``__VALID__`` <= '1;                                                                           \
     do @ (posedge ``__clk__``);                                                                    \
     while (``__READY__`` !== '1);                                                                  \
     ``__VALID__`` <= '0;                                                                           \
+    ``__NAME__``_send_queue.delete(0);                                                             \
   endtask                                                                                          \
+  /*--------------------------------------------------------------------------------------------*/ \
+  int ``__NAME__``_recv_id = 0;                                                                    \
+  int ``__NAME__``_recv_queue  [$];                                                                \
   task automatic recv_``__NAME__``(output ``__TYPE__`` beat);                                      \
+    int thread_id;                                                                                 \
+    thread_id = ``__NAME__``_recv_id;                                                              \
+    ``__NAME__``_recv_id++;                                                                        \
+    ``__NAME__``_recv_queue.push_back(thread_id);                                                  \
+    while (``__NAME__``_recv_queue[0] != thread_id) @(posedge ``__clk__``);                        \
     ``__READY__`` <= '1;                                                                           \
     do @ (posedge ``__clk__``);                                                                    \
     while (``__VALID__`` !== '1);                                                                  \
     beat = ``__CHAN__``;                                                                           \
     ``__READY__`` <= '0;                                                                           \
+    ``__NAME__``_recv_queue.delete(0);                                                             \
   endtask                                                                                          \
+  /*--------------------------------------------------------------------------------------------*/ \
+  int ``__NAME__``_look_id = 0;                                                                    \
+  int ``__NAME__``_look_queue  [$];                                                                \
   task automatic look_``__NAME__``(output ``__TYPE__`` beat);                                      \
+    int thread_id;                                                                                 \
+    thread_id = ``__NAME__``_look_id;                                                              \
+    ``__NAME__``_look_id++;                                                                        \
+    ``__NAME__``_look_queue.push_back(thread_id);                                                  \
+    while (``__NAME__``_look_queue[0] != thread_id) @(posedge ``__clk__``);                        \
     do @ (posedge ``__clk__``);                                                                    \
     while (``__VALID__`` !== '1 || ``__READY__`` !== '1);                                          \
     beat = ``__CHAN__``;                                                                           \
+    ``__NAME__``_look_queue.delete(0);                                                             \
   endtask                                                                                          \
+  /*--------------------------------------------------------------------------------------------*/ \
 
 
 `define VALID_ONLY_DRIVE_CATCH(__NAME__, __TYPE__, __clk__, __CHAN__, __VALID__)                   \
+  /*--------------------------------------------------------------------------------------------*/ \
+  int ``__NAME__``_drive_id = 0;                                                                   \
+  int ``__NAME__``_drive_queue  [$];                                                               \
   task automatic drive_``__NAME__``(input ``__TYPE__`` beat);                                      \
+    int thread_id;                                                                                 \
+    thread_id = ``__NAME__``_drive_id;                                                             \
+    ``__NAME__``_drive_id++;                                                                       \
+    ``__NAME__``_drive_queue.push_back(thread_id);                                                 \
+    while (``__NAME__``_drive_queue[0] != thread_id) @(posedge ``__clk__``);                       \
     ``__CHAN__``  <= beat;                                                                         \
     ``__VALID__`` <= '1;                                                                           \
     @ (posedge ``__clk__``);                                                                       \
     ``__VALID__`` <= '0;                                                                           \
+    ``__NAME__``_drive_queue.delete(0);                                                            \
   endtask                                                                                          \
+  /*--------------------------------------------------------------------------------------------*/ \
+  int ``__NAME__``_catch_id = 0;                                                                   \
+  int ``__NAME__``_catch_queue  [$];                                                               \
   task automatic catch_``__NAME__``(output ``__TYPE__`` beat);                                     \
+    int thread_id;                                                                                 \
+    thread_id = ``__NAME__``_catch_id;                                                             \
+    ``__NAME__``_catch_id++;                                                                       \
+    ``__NAME__``_catch_queue.push_back(thread_id);                                                 \
+    while (``__NAME__``_catch_queue[0] != thread_id) @(posedge ``__clk__``);                       \
     do @ (posedge ``__clk__``);                                                                    \
     while (``__VALID__`` !== '1);                                                                  \
     beat = ``__CHAN__``;                                                                           \
+    ``__NAME__``_catch_queue.delete(0);                                                            \
   endtask                                                                                          \
+  /*--------------------------------------------------------------------------------------------*/ \
 
--- include/vip/tb_ess.sv
+++ include/vip/tb_ess.sv
@@ -1,8 +1,8 @@
 // ### Author : Foez Ahmed (foez.official@gmail.com)
 
 `ifndef CONGIF
-  `define CONFIG "default"
-`endif // CONGIF
+`define CONFIG "default"
+`endif  // CONGIF
 
 `define CREATE_CLK(__CLK__, __HIGH__, __LOW__)                                                     \
   bit ``__CLK__`` = '1;                                                                            \
@@ -23,6 +23,7 @@ initial begin
   $dumpfile("dump.vcd");
   $dumpvars;
 `endif  // ENABLE_DUMPFILE
+  $timeformat(-6, 3, "us");
   repeat (1000) repeat (1000) repeat (1000) #1000;
   result_print(0, $sformatf("\033[1;31m[FATAL][TIMEOUT]\033[0m"));
   $finish;
@@ -33,7 +34,7 @@ final begin
 end
 
 function automatic void result_print(bit PASS, string msg);
-  if (PASS==0) $sformat(msg, "\033[1;31m[FAIL]\033[0m %s", msg);
+  if (PASS == 0) $sformat(msg, "\033[1;31m[FAIL]\033[0m %s", msg);
   else $sformat(msg, "\033[1;32m[PASS]\033[0m %s", msg);
   $sformat(msg, "%s \033[1;33m[%s]\033[0m", msg, top_module_name);
   $display(msg);
