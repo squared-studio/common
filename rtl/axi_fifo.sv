0512b4ce4f9761d4cd8083d4e2b683c7f4faf54f
Foez Ahmed <foez.official@gmail.com>
2023-10-27 19:30:22 +0600

    modified:   rtl/axi_fifo.sv

--- rtl/axi_fifo.sv
+++ rtl/axi_fifo.sv
@@ -1,11 +1,6 @@
 // Description
 // ### Author : Foez Ahmed (foez.official@gmail.com)
 
-//`include "addr_map.svh"
-//`include "axi4l_assign.svh"
-//`include "axi4l_typedef.svh"
-//`include "axi4_assign.svh"
-//`include "axi4_typedef.svh"
 `include "default_param_pkg.sv"
 
 module axi_fifo #(
