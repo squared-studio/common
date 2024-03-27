308874117cb017936631e3a23781545aa9e9a799
Foez Ahmed <foez.official@gmail.com>
2024-03-27 10:55:23 +0600

    modified:   tb/xbar_tb/xbar_tb.sv

--- tb/xbar_tb/xbar_tb.sv
+++ tb/xbar_tb/xbar_tb.sv
@@ -70,8 +70,7 @@ module xbar_tb;
       end
     end
 
-    result_print(error, "CrossBar verification");
-    // result_print(!error, "CrossBar verification");
+    result_print(!error, "CrossBar verification");
 
     $finish;
 
