9e0301148a6c5e25103382ac1a105d89c5663294
Foez Ahmed <foez.official@gmail.com>
2024-04-07 12:04:21 +0600

    modified:   inc/riscv_pkg.sv modified:   rtl/rv_id.sv

--- rtl/rv_id.sv
+++ rtl/rv_id.sv
@@ -15,11 +15,14 @@ Author : Foez Ahmed (foez.official@gmail.com)
 module rv_id
   import riscv_pkg::*;
 #(
-    parameter bit RV32I = 0,
-    parameter bit RV64I = 0
+    parameter bit RV32I = 1,
+    parameter bit RV64I = 1,
+    parameter bit RVF   = 1,
+    parameter bit RVD   = 1,
+    parameter bit RVQ   = 1
 ) (
-    input  logic           [31:0] instr_i,
-    output decoded_instr_t        decode_o
+    input  logic          [31:0] inst_i,
+    output decoded_inst_t        inst_o
 );
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -34,35 +37,67 @@ module rv_id
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
   always_comb begin
-    decode_o = '0;
-    case (instr_i[6:0])
+    inst_o.imm = imm;
+    case (imm_len)
+      default: for (int i = ImmLen; i < ImmLen; i++) inst_o.imm[i] = inst_o.imm[i-1];
+      12: for (int i = 12; i < ImmLen; i++) inst_o.imm[i] = inst_o.imm[i-1];
+      13: for (int i = 13; i < ImmLen; i++) inst_o.imm[i] = inst_o.imm[i-1];
+      20: for (int i = 20; i < ImmLen; i++) inst_o.imm[i] = inst_o.imm[i-1];
+    endcase
+  end
 
-      'h03: begin
-        imm_len = 12;
-        imm = instr_i[31:20];
-        decode_o.rs1 = instr_i[19:15];
-        decode_o.rd = instr_i[11:7];
-        case (instr_i[14:12])
-          0: decode_o.func = LB;
-          1: decode_o.func = LH;
-          2: decode_o.func = LW;
-          4: decode_o.func = LBU;
-          5: decode_o.func = LHU;
+  always_comb begin
+    inst_o = '0;
+    case (inst_i[6:0])
+
+      default: begin
+      end
+
+      'h03: begin : LOAD
+        imm_len    = 12;
+        imm        = inst_i[31:20];
+        inst_o.rs1 = inst_i[19:15];
+        inst_o.rd  = inst_i[11:7];
+        case (inst_i[14:12])
+          0: if (RV32I) inst_o.func = LB;
+          1: if (RV32I) inst_o.func = LH;
+          2: if (RV32I) inst_o.func = LW;
+          3: if (RV64I) inst_o.func = LD;
+          4: if (RV32I) inst_o.func = LBU;
+          5: if (RV32I) inst_o.func = LHU;
+          6: if (RV64I) inst_o.func = LWU;
           default: begin
-            if (RV64I) begin
-              case (instr_i[14:12])
-                3: decode_o.func = LD;
-                6: decode_o.func = LWU;
-                default: begin
-                end
-              endcase
-            end
           end
         endcase
       end
 
-      default: begin
+      'h07: begin : LOAD_FP
+        imm_len    = 12;
+        imm        = inst_i[31:20];
+        inst_o.rs1 = inst_i[19:15];
+        inst_o.rd  = inst_i[11:7];
+        case (inst_i[14:12])
+          2: if (RVF) inst_o.func = FLW;
+          3: if (RVD) inst_o.func = FLD;
+          4: if (RVQ) inst_o.func = FLQ;
+          default: begin
+          end
+        endcase
       end
+
+      'h0F: begin : MISC_MEM
+        inst_o.fm   = inst_i[31:28];
+        inst_o.pred = inst_i[27:24];
+        inst_o.succ = inst_i[23:20];
+        inst_o.rs1  = inst_i[19:15];
+        inst_o.rd   = inst_i[11:7];
+        case (inst_i[14:12])
+          0: if (RV32I) inst_o.func = FENCE;
+          default: begin
+          end
+        endcase
+      end
+
 
     endcase
   end
