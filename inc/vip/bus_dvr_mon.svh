8ab9795f0f44f3cbe87ade352169535bf5cf0ee1
Foez Ahmed <foez.official@gmail.com>
2024-11-16 23:04:16 +0600

    modified:   inc/vip/bus_dvr_mon.svh

--- inc/vip/bus_dvr_mon.svh
+++ inc/vip/bus_dvr_mon.svh
@@ -1,9 +1,9 @@
 // ### Author : Foez Ahmed (foez.official@gmail.com)
 
 `ifndef BUS_DVR_MON_SVH
-`define BUS_DVR_MON_SVH
+`define BUS_DVR_MON_SVH 0
 
-`define HANDSHAKE_SEND_RECV_LOOK(__NM__, __TP__, __CLK__, __ARST_N__, __CHAN__, __VAL__, __RDY__) \
+`define HANDSHAKE_SEND_RECV_LOOK(__NM__, __TP__, __CLK__, __ARST_N__, __BUS__, __VAL__, __RDY__)  \
                                                                                                   \
   int ``__NM__``_send_id = 0;                                                                     \
   int ``__NM__``_send_queue  [$];                                                                 \
@@ -12,9 +12,10 @@
     thread_id = ``__NM__``_send_id;                                                               \
     ``__NM__``_send_id++;                                                                         \
     ``__NM__``_send_queue.push_back(thread_id);                                                   \
-    while ((``__NM__``_send_queue[0] != thread_id) && (__ARST_N__ == 1)) @(posedge ``__CLK__``);  \
+    while ((``__NM__``_send_queue[0] != thread_id) && (__ARST_N__ == 1))                          \
+      @(posedge ``__CLK__`` or negedge ``__ARST_N__``);                                           \
     if (__ARST_N__) begin                                                                         \
-      ``__CHAN__``  <= beat;                                                                      \
+      ``__BUS__``  <= beat;                                                                       \
       ``__VAL__`` <= '1;                                                                          \
       do @ (posedge ``__CLK__``);                                                                 \
       while ((``__RDY__`` !== '1) && (__ARST_N__ == 1));                                          \
@@ -30,12 +31,13 @@
     thread_id = ``__NM__``_recv_id;                                                               \
     ``__NM__``_recv_id++;                                                                         \
     ``__NM__``_recv_queue.push_back(thread_id);                                                   \
-    while ((``__NM__``_recv_queue[0] != thread_id) && (__ARST_N__ == 1)) @(posedge ``__CLK__``);  \
+    while ((``__NM__``_recv_queue[0] != thread_id) && (__ARST_N__ == 1))                          \
+      @(posedge ``__CLK__`` or negedge ``__ARST_N__``);                                           \
     if (__ARST_N__) begin                                                                         \
       ``__RDY__`` <= '1;                                                                          \
       do @ (posedge ``__CLK__``);                                                                 \
       while ((``__VAL__`` !== '1) && (__ARST_N__ == 1));                                          \
-      beat = ``__CHAN__``;                                                                        \
+      beat = ``__BUS__``;                                                                         \
       ``__RDY__`` <= '0;                                                                          \
     end                                                                                           \
     ``__NM__``_recv_queue.delete(0);                                                              \
@@ -48,18 +50,19 @@
     thread_id = ``__NM__``_look_id;                                                               \
     ``__NM__``_look_id++;                                                                         \
     ``__NM__``_look_queue.push_back(thread_id);                                                   \
-    while ((``__NM__``_look_queue[0] != thread_id) && (__ARST_N__ == 1)) @(posedge ``__CLK__``);  \
+    while ((``__NM__``_look_queue[0] != thread_id) && (__ARST_N__ == 1))                          \
+      @(posedge ``__CLK__`` or negedge ``__ARST_N__``);                                           \
     if (__ARST_N__) begin                                                                         \
       do @ (posedge ``__CLK__``);                                                                 \
       while ((``__VAL__`` !== '1 || ``__RDY__`` !== '1) && (__ARST_N__ == 1));                    \
-      beat = ``__CHAN__``;                                                                        \
+      beat = ``__BUS__``;                                                                         \
     end                                                                                           \
     ``__NM__``_look_queue.delete(0);                                                              \
   endtask                                                                                         \
                                                                                                   \
 
 
-`define VALID_ONLY_DRIVE_CATCH(__NM__, __TP__, __CLK__,  __ARST_N__, __CHAN__, __VAL__)           \
+`define VALID_ONLY_DRIVE_CATCH(__NM__, __TP__, __CLK__, __ARST_N__, __BUS__, __VAL__)             \
                                                                                                   \
   int ``__NM__``_drive_id = 0;                                                                    \
   int ``__NM__``_drive_queue  [$];                                                                \
@@ -68,9 +71,10 @@
     thread_id = ``__NM__``_drive_id;                                                              \
     ``__NM__``_drive_id++;                                                                        \
     ``__NM__``_drive_queue.push_back(thread_id);                                                  \
-    while ((``__NM__``_drive_queue[0] != thread_id) && (__ARST_N__ == 1)) @(posedge ``__CLK__``); \
+    while ((``__NM__``_drive_queue[0] != thread_id) && (__ARST_N__ == 1))                         \
+      @(posedge ``__CLK__`` or negedge ``__ARST_N__``);                                           \
     if (__ARST_N__) begin                                                                         \
-      ``__CHAN__``  <= beat;                                                                      \
+      ``__BUS__``  <= beat;                                                                       \
       ``__VAL__`` <= '1;                                                                          \
       @ (posedge ``__CLK__``);                                                                    \
       ``__VAL__`` <= '0;                                                                          \
@@ -85,11 +89,12 @@
     thread_id = ``__NM__``_catch_id;                                                              \
     ``__NM__``_catch_id++;                                                                        \
     ``__NM__``_catch_queue.push_back(thread_id);                                                  \
-    while ((``__NM__``_catch_queue[0] != thread_id) && (__ARST_N__ == 1)) @(posedge ``__CLK__``); \
+    while ((``__NM__``_catch_queue[0] != thread_id) && (__ARST_N__ == 1))                         \
+      @(posedge ``__CLK__`` or negedge ``__ARST_N__``);                                           \
     if (__ARST_N__) begin                                                                         \
       do @ (posedge ``__CLK__``);                                                                 \
       while ((``__VAL__`` !== '1) && (__ARST_N__ == 1));                                          \
-      beat = ``__CHAN__``;                                                                        \
+      beat = ``__BUS__``;                                                                         \
     end                                                                                           \
     ``__NM__``_catch_queue.delete(0);                                                             \
   endtask                                                                                         \
